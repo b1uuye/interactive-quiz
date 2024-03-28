041a128739acb2c1d358f69682276c296b02c3a0
b1uuye <106692162+b1uuye@users.noreply.github.com>
2024-03-28 23:05:05 +0000

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,115 @@
+//creating an array and passing the number, questions, options and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: "Who has the most NBA championship Rings?",
+        answer: "Bill Russell",
+        options: [
+            "Kobe Bryant",
+            "Michael Jordan",
+            "Bill Russell",
+            "Lebron James"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does the NBA stand for?",
+        answer: "National Basketball Association",
+        options: [
+            "National Bowling Association",
+            "National Badmintion Associaition",
+            "National Baby Association",
+            "National Basketball Association"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which player has the most career blocks?",
+        answer: "Hakeem Olajuwon",
+        options: [
+            "Hakeem Olajuwon",
+            "Tim Duncan",
+            "Dikembe Mutombo",
+            "Kareem Adbul-Jabbar"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which player has made the most career 3pts?",
+        answer: "Stephen Curry",
+        options: [
+            "Ray Allen",
+            "Stephen Curry",
+            "Damian Lillard",
+            "Reggie Miller"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the most amount of points scored in a game?",
+        answer: "100",
+        options: [
+            "81",
+            "73",
+            "69",
+            "100"
+        ]
+    },
+    {
+        numb: 6,
+        question: "How many NBA teams are there?",
+        answer: "30",
+        options: [
+            "28",
+            "29",
+            "30",
+            "32"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Who is the shortest ever recorded player in the NBA?",
+        answer: "Muggsy Bogues",
+        options: [
+            "Muggsy Bogues",
+            "Nate Robinson",
+            "Isaiah Thomas",
+            "Kay Felder"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the maximum time a player can hold onto the ball, after passing the half court?",
+        answer: "5 seconds",
+        options: [
+            "7 seconds",
+            "10 seconds",
+            "3 seconds",
+            "5 seconds"
+        ]
+    },
+    {
+        numb: 9,
+        question: "How many fouls does it take for a player to foul out in the NBA?",
+        answer: "6",
+        options: [
+            "5",
+            "6",
+            "7",
+            "10"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Who is the greatest player of all time?",
+        answer: "Lebron James",
+        options: [
+            "Kobe Bryant",
+            "Lebron James",
+            "Bill Russell",
+            "Michael Jordan"
+        ]
+    },
+
+]
